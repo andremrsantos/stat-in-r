1b1d90e355ecafcb7fe5ec4b8a68a63020c92137
Andre M Ribeiro dos Santos <andremrsantos@gmail.com>
2017-04-12 21:28:11 -0300

    Created generate-data.r for anova and produced two datasets

---
+++ anova/generate-data.r
@@ -0,0 +1,27 @@
+#! /usr/bin/env Rscript --vanilla
+
+set.seed(551991)
+
+n <- 64
+
+## Generate in class exercises dataset
+sex  <- sample(c("M", "F"), n, replace = T)
+drug <- rep(c("P", "A", "B", "C"), each=n/4)
+bp   <- rnorm(n, 100 - 10 * (sex=="F"), 3)
+bp[drug=="A"] <- bp[drug=="A"] - 12
+bp[drug=="C"] <- bp[drug=="C"] - (15 + 7 * (sex[drug=="C"]=="F"))
+
+write.table(data.frame(sex, drug, bp),
+            'hbp-treatment.tsv',
+            sep='\t', quote=F, row.names=F)
+
+## Generate homework exercise dataset
+dose <- factor(rep(1:4, each=n/4), labels=c("0mg", "5mg", "10mg", "50mg"))
+sup  <- rep(c("none", "vitC"), n/2)
+
+growth <- rnorm(n, 100 - 7.2 * as.numeric(dose), 7)
+growth[sup=="vitC"] <- growth[sup=="vitC"] + 4.3 * as.numeric(dose[sup=="vitC"])
+
+write.table(data.frame(dose, sup, growth),
+            "tumor-treatment.tsv",
+            sep="\t", quote=F, row.names=F)
