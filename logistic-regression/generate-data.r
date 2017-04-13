8b5c77961eaf7a0ce9f048cc634f74857221de2b
Andre M Ribeiro dos Santos <andremrsantos@gmail.com>
2017-04-13 13:36:44 -0300

    Created generate-data.r to produce toy dataset

---
+++ logistic-regression/generate-data.r
@@ -0,0 +1,30 @@
+#! /usr/bin/env Rscript --vanilla
+
+set.seed(551991)
+
+n <- 120
+
+## Generate dataset for in class exercise
+hw <- function(p) c(p^2, 2 * p * (1-p), (1-p)^2)
+
+sex   <- sample(c("M", "F"), n, replace = T)
+cigar <- sample(c("N", "S"), n, replace = T, prob = c(0.9, 0.15))
+cdh1  <- sample(c("11", "12", "22"), n, replace = T, prob = hw(0.35))
+tp53  <- sample(c("11", "12", "22"), n, replace = T, prob = hw(0.15))
+
+simulate_anc <- function(n = 100, prob = c(0.25, 0.6, 0.15))
+    prop.table(table(sample(c("afr", "eur", "amr"), n, replace = T,
+                            prob = prob)))
+anc   <- replicate(n, simulate_anc())
+afr   <- anc["afr",]
+eur   <- anc["eur",]
+amr   <- anc["amr",]
+
+
+lodds <- rnorm(n, 0.5 * (sex == "F") + 2 * (cigar == "S") +
+                  3 * (cdh1 == "11") - 2.5 * (tp53 != "22") +
+                  10 * amr - 1)
+
+write.table(data.frame(sex, cigar, amr, eur, afr, cdh1, tp53),
+            'cancer-mama.tsv',
+            sep = "\t", quote = FALSE, row.names = F)
